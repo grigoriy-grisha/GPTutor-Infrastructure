b5951dc7a3ee6cb61567e3286ed8887c51b7b9e7
Григорий Гаврилов <lowww1337@mail.ru>
2023-06-30 15:23:28 +0300

    Добавит конфиги для stage платформы

--- deploy-all.sh
+++ deploy-all.sh
@@ -15,3 +15,17 @@ cd ../
 docker-compose down
 docker-compose build
 docker-compose up -d
+
+docker-compose up frontend -d
+docker-compose up frontend-stage -d
+
+docker-compose up nginx -d
+
+docker-compose up postgresql -d
+docker-compose up postgresql-stage -d
+
+docker-compose up certbot -d
+docker-compose up certbot-stage -d
+
+docker-compose up backend -d
+docker-compose up backend-stage -d
