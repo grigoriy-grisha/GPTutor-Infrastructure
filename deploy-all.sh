973fd2ef10fbff28888ec93a445693c5ec117e22
Григорий Гаврилов <lowww1337@mail.ru>
2023-06-30 16:17:10 +0300

    Добавит конфиги для stage платформы

--- deploy-all.sh
+++ deploy-all.sh
@@ -4,27 +4,41 @@ sh .env.sh
 
 cd ./GPTutor-Frontend || exit
 git pull
+git checkout origin main
 
 cd ../
 
 cd ./GPTutor-Backend || exit
 git pull
+git checkout origin main
 
 cd ../
 
 docker-compose down
-docker-compose build
 
 docker-compose up -d frontend
-docker-compose up -d frontend-stage
-
 docker-compose up -d nginx
-
 docker-compose up -d postgresql
-docker-compose up -d postgresql-stage
-
+docker-compose up -d backend
 docker-compose up -d certbot
+
+sh .env.sh
+
+cd ./GPTutor-Frontend || exit
+git pull
+git checkout origin develop
+
+cd ../
+
+cd ./GPTutor-Backend || exit
+git pull
+git checkout origin develop
+
+cd ../
+
+docker-compose up -d frontend-stage
+docker-compose up -d postgresql-stage
+docker-compose up -d backend-stage
 docker-compose up -d certbot-stage
 
-docker-compose up -d backend
-docker-compose up -d backend-stage
+
